561374b27d5330b7dcfbccb9e492780fae95e646
Yu-Wei Chang <andy.yu29@gmail.com>
2022-07-19 20:20:02 +0800

    Subscribe joint position from kinova

--- ros_unity_test/Assets/Scripts/RobotControl.cs
+++ ros_unity_test/Assets/Scripts/RobotControl.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
-using Unity.Robotics;
 using UnityEngine;
-
+using Unity.Robotics.ROSTCPConnector;
+using RosJointPos = RosMessageTypes.KinovaTest.KinovaMsgMsg;
 namespace MyController
 {
     public class RobotControl : MonoBehaviour
@@ -14,10 +14,13 @@ namespace MyController
         public float speed = 5f; // Units: degree/s
         public float torque = 100f; // Units: Nm or N
         public float acceleration = 5f;// Units: m/s^2 / degree/s^2
+        [SerializeField] bool RosEnable = false;
 
-        private float[] home = { 0f, 15f, 180f, -130f, 0f, 55f, 90f };
+        private int[] home = { 0, 15, 180, -130, 0, 55, 90 };
+        private int[] currentPos = new int[7];
         void Start()
         {
+            ROSConnection.GetOrCreateInstance().Subscribe<RosJointPos>("jointPos", PosChange);
             articulationChain = this.GetComponentsInChildren<ArticulationBody>();
             int defDyanmicVal = 10;
             foreach (ArticulationBody joint in articulationChain)
@@ -26,12 +29,14 @@ namespace MyController
                 joint.jointFriction = defDyanmicVal;
                 joint.angularDamping = defDyanmicVal;
             }
-
         }
 
         void Update()
         {
-            StartCoroutine(DelayFunc());
+            if (RosEnable)
+                StartCoroutine(DelayFunc(currentPos));
+            else
+                StartCoroutine(DelayFunc(home));
         }
 
         public void UpdatePosition(JointPosition joint, float angle)
@@ -42,15 +47,31 @@ namespace MyController
             drive.target = angle;
             joint.joint.xDrive = drive;
         }
-        IEnumerator DelayFunc()
+        IEnumerator DelayFunc(int[] angle)
         {
-            WaitForSeconds wait = new WaitForSeconds(1);
+            WaitForSeconds wait = new WaitForSeconds(0.001f);
             for (int i = 1; i < 8; i++)
             {
                 JointPosition joint = articulationChain[i].GetComponent<JointPosition>();
-                UpdatePosition(joint, home[i - 1]);
+                UpdatePosition(joint, angle[i - 1]);
                 yield return wait;
             }
+        }
+
+        private void PosChange(RosJointPos posMessage)
+        {
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (posMessage.jointPos[i] >= 360)
+                    posMessage.jointPos[i] = 0;
+                if (i == 3)
+                    currentPos[i] = (int)posMessage.jointPos[i] - 360;
+                else
+                    currentPos[i] = (int)posMessage.jointPos[i];
+                Debug.Log(currentPos[i]);
+            }
+
         }
     }
 }
