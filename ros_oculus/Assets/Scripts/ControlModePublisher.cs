618ffcab2e26176d991035226b61ebd05a53f677
Yu-Wei Chang <YW-Chang@NRSL.com>
2023-03-19 14:25:26 +0800

    Publish the controlMode based on the Primary button

---
+++ ros_oculus/Assets/Scripts/ControlModePublisher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosBoolMsg = RosMessageTypes.Std.BoolMsg;
+public class ControlModePublisher : MonoBehaviour
+{
+    [SerializeField] HandController handController;
+
+    [SerializeField] float publishMsgFreq = 0.5f;
+    private ROSConnection ros;
+
+    private string topicname = "controlMode";
+
+    private float timeElapsed;
+
+    void Start()
+    {
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<RosBoolMsg>(topicname);
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > publishMsgFreq)
+        {
+            RosBoolMsg msg = new RosBoolMsg((handController.GetPressedCount() % 2) != 0);
+            ros.Publish(topicname, msg);
+            timeElapsed = 0;
+        }
+    }
+}
