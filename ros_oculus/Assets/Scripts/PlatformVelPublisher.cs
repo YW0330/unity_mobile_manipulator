58b89e16c15afdce7c2abd731eba6371f250662a
Yu-Wei Chang <YW-Chang@NRSL.com>
2023-06-05 02:13:06 +0800

    Adjust the velocity gain of platform

--- ros_oculus/Assets/Scripts/PlatformVelPublisher.cs
+++ ros_oculus/Assets/Scripts/PlatformVelPublisher.cs
@@ -44,11 +44,11 @@ public class PlatformVelPublisher : MonoBehaviour
 
     private void SetTwist(float linear_x, float angular_z)
     {
-        msg.linear.x = Mathf.Abs(linear_x) > 0.5 ? linear_x : 0;
+        msg.linear.x = Mathf.Abs(linear_x) > 0.5 ? linear_x * 0.15f : 0;
         msg.linear.y = 0;
         msg.linear.z = 0;
         msg.angular.x = 0;
         msg.angular.y = 0;
-        msg.angular.z = Mathf.Abs(angular_z) > 0.5 ? -angular_z : 0;
+        msg.angular.z = Mathf.Abs(angular_z) > 0.5 ? -(angular_z * 0.2f) : 0;
     }
 }
